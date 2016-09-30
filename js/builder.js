9e7f0218a8b098b0fc2748d0f798fb01710db380
Theresa Summa Lurie <theresa.summa@gsa.gov>
2016-09-30 11:44:38 -0700

    cleaning up builder js

--- js/builder.js
+++ js/builder.js
@@ -7,32 +7,38 @@ $("#form").alpaca({
           "submit": {
             "title": "Add project",
             "click": function () {
-                var val = this.getValue(),
-                    projects = val.projects;
-                val.projects = [];
-                val.projects.push(projects);
-                if (this.isValid(true)) {
-                  if ($("#results").html() !== "") {
-                    var results = JSON.stringify(val, null, "  "),
-                        existingJSON = $("#results").html().slice(0, -9) + "  },";
-                    $("#results").html(existingJSON);
-                    results = results.substring(results.indexOf("[") + 1);
-                    $("#results").append(results);
-                   }
-                   else {
-                    var results = JSON.stringify(val, null, "  ");
-                    $("#results").append(results);
-                    $("#download").removeAttr('disabled');
-                    $("#download_link").attr({
-                      'href': 'data:text/json;charset=utf-8,' + encodeURIComponent(results),
-                      'target': '_blank',
-                      'download': 'civic.json'                          
-                    });
+              var val = this.getValue(),
+                projects = val.projects;
+              val.projects = [];
+              val.projects.push(projects);
+              if (this.isValid(true)) {
+                if ($("#results").html() !== "") {
+                  var results = JSON.stringify(val, null, "  "),
+                      existingJSON = $("#results").html().slice(0, -9) + "  },";
+                  $("#results").html(existingJSON);
+                  results = results.substring(results.indexOf("[") + 1);
+                  $("#results").append(results);
+                  $("#download_link").attr({
+                    'href': 'data:text/json;charset=utf-8,' + encodeURIComponent($('#results').text()),
+                    'target': '_blank',
+                    'download': 'code.json'                          
+                  });
+                }
+                else {
+                  var results = JSON.stringify(val, null, "  ");
+                  $("#results").append(results);
+                  $("#download").removeAttr('disabled');
+                  $("#download_link").attr({
+                    'href': 'data:text/json;charset=utf-8,' + encodeURIComponent(results),
+                    'target': '_blank',
+                    'download': 'code.json'                          
+                  });
                 }                    
               }
             }
           }
         }
       }
-  }});
+    }
+  });
 });
